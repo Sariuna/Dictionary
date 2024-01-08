6fc687fd68985ab4dbc30cb66827c69a4bfd8c3c
Sariuna <sariuna.bazarova.00@mail.ru>
2024-01-08 12:52:06 +0300

    ferst commit

---
+++ src/main.jsx
@@ -0,0 +1,66 @@
+import { TermList } from './TermList';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+
+function saveTermList(terms) {
+  localStorage.setItem("termList", JSON.stringify(terms));
+}
+
+function restoreTermList() {
+
+  const rawTermList = localStorage.getItem("termList");
+
+  if (!rawTermList) {
+    return []
+  }
+
+  return JSON.parse(rawTermList);
+}
+
+let terms = restoreTermList();
+
+const descriptionList = document.getElementById("description-list");
+
+const reactRoot = createRoot(descriptionList);
+
+function syncTermList() {
+  
+  saveTermList(terms);
+  reactRoot.render(<TermList terms={terms} onDelete={deleteItem} />);
+}
+
+function addTerm(title, description) {
+
+  terms.push({
+    id: Date.now(),
+    title,
+    description
+  });
+
+  terms.sort((term1, term2) => (term1.title < term2.title ? -1 : 1));
+
+  syncTermList();
+}
+
+function deleteItem(id) {
+
+  terms = terms.filter((term) => term.id !== id);
+
+  syncTermList();
+}
+
+syncTermList();
+
+const form = document.getElementById('add-description');
+
+form.addEventListener('submit', (event) => {
+
+  event.preventDefault();
+
+  const title = form.elements['title'].value;
+  const description = form.elements['description'].value;
+
+  form.reset();
+
+  addTerm(title, description);
+});
